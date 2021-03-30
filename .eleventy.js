ed084cf9bf71104b7065eb14ea008994a72c1838
Thi <dinhanhthi@gmail.com>
2021-03-30 10:10:57 +0200

    wip dark

--- .eleventy.js
+++ .eleventy.js
@@ -33,7 +33,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(require("eleventy-plugin-nesting-toc"), {
     tags: ["h2", "h3", "h4"], // Which heading tags are selected (headings must each have an ID attribute)
     wrapper: "div", // Element to put around the root `ol`
-    wrapperClass: "toc", // Class for the element around the root `ol`
+    wrapperClass: "toc toc-common", // Class for the element around the root `ol`
     headingText: "", // Optional text to show in heading above the wrapper element
     headingTag: "h2", // Heading tag when showing heading above the wrapper element
   });
